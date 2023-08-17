a58619f0bf33ce4433b7709243c3472b4a28dea9
Jayamvin <vin.rani@gmail.com>
2023-08-17 10:35:37 -0700

    added env files

--- src/main/java/com/qa/gorest/configuration/ConfigurationManager.java
+++ src/main/java/com/qa/gorest/configuration/ConfigurationManager.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import com.qa.gorest.framworkexception.APIFrameworkException;
+
 public class ConfigurationManager {
 	
 	private Properties prop;
@@ -12,17 +14,56 @@ public class ConfigurationManager {
 
 	public Properties initProp() {
 		prop = new Properties();
-		try {
-			ip = new FileInputStream("./src/test/resources/config/config.properties");
-			prop.load(ip);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		
+		// maven: cmd line argument:
+				// mvn clean install -Denv="qa"
+				// mvn clean install
 
-		return prop;
+				String envName = System.getProperty("env");
 
-	}
+				try {
+					if (envName == null) {
+						System.out.println("no env is given...hence running tests on QA env... ");
+						ip = new FileInputStream("./src/test/resources/config/qa.config.properties");
+					} else {
+						System.out.println("Running tests on env: " + envName);
+
+						switch (envName.toLowerCase().trim()) {
+						case "qa":
+							ip = new FileInputStream("./src/test/resources/config/qa.config.properties");
+							break;
+						case "dev":
+							ip = new FileInputStream("./src/test/resources/config/dev.config.properties");
+							break;
+						case "stage":
+							ip = new FileInputStream("./src/test/resources/config/stage.config.properties");
+							break;
+						case "prod":
+							ip = new FileInputStream("./src/test/resources/config/prod.config.properties");
+							break;
+
+						default:
+							System.out.println("Please pass the right env name..." + envName);
+							throw new APIFrameworkException("WRONG ENV IS Given");
+						}
+					}
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+
+				try
+
+				{
+					prop.load(ip);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				return prop;
+
+			}
+
+		
+		
 
 }
